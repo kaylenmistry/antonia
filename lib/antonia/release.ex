d0bc108a5577cbc562a5621c2c89ccf5a2ea9c68
Kaylen Mistry <kaylenmistry@gmail.com>
2025-10-13 10:45:12 +0100

    restore release.ex

--- lib/antonia/release.ex
+++ lib/antonia/release.ex
@@ -10,20 +10,19 @@ defmodule Antonia.Release do
   def migrate do
     load_app()
 
-    # for repo <- repos() do
-    #   {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
-    # end
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    end
   end
-
   @doc false
   def rollback(repo, version) do
     load_app()
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
-  # defp repos do
-  #   Application.fetch_env!(@app, :ecto_repos)
-  # end
+  defp repos do
+    Application.fetch_env!(@app, :ecto_repos)
+  end
 
   defp load_app do
     Application.ensure_all_started(:ssl)
